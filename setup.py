f86cc9fec8283413292a9b4c24f57a783aefbed7
ramyakoth <kothwalramyareddy@gmail.com>
2016-03-01 21:23:16 +0530

    This is my 4th commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup (name             = 'Falcon',
+       version          = '0.0.1',
+       description      = 'Falcon',
+       url              = 'https://github.com/EINDHANLTD/PROJECT-FALCON',
+       author           = 'Eindhan Ltd',
+       packages         = ['falcon'],
+       zip_safe         = False)
+        
